2a68b58bc9f548e744beedfbceaa9a3e1d994dfe
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-03-24 02:21:44 +0530

    docker configuration

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 const server = http.createServer(app);
 app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res) => {});
+// app.get("/", (req, res) => {});
+const io = require("socket.io")(server);
+io.on("connection", (socket) => {
+  console.log(`Socket Connected-${socket.id}`);
+  socket.on("binarydata", (stream) => {
+    console.log(`Binary Stream Comming...${stream}`);
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`[Server Running...${PORT}]`);
--- public/script.js
+++ public/script.js
@@ -1,9 +1,30 @@
 const userVideo = document.getElementById("video");
+const startButton = document.getElementById("start-button");
+
+const state = { media: null };
+const socket = io();
+startButton.addEventListener("click", () => {
+  const mediaRecorder = new MediaRecorder(state.media, {
+    audioBitsPerSecond: 128000,
+    videoBitsPerSecond: 2500000,
+    framerate: 25,
+  });
+  mediaRecorder.ondataavailable = (event) => {
+    console.log("Binary Data", event.data);
+    socket.emit("binarydata", event.data);
+  };
+  mediaRecorder.start(25);
+});
 
 window.addEventListener("load", async (e) => {
-  const media = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  });
-  userVideo.srcObject = media;
+  try {
+    const media = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    state.media = media;
+    userVideo.srcObject = media;
+  } catch (err) {
+    console.error("Error accessing user media:", err);
+  }
 });
