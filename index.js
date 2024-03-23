a2daac8cba58cd9bdf2b550c410cc20f32d82666
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-03-24 02:57:09 +0530

    setup in docker

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const options = [
   '-c:v', 'libx264',
   '-preset', 'ultrafast',
   '-tune', 'zerolatency',
-  '-r', '25',
-  '-g', '50',
-  '-keyint_min', '25',
+  '-r', `${25}`,
+  '-g', `${25 * 2}`,
+  '-keyint_min', 25,
   '-crf', '25',
   '-pix_fmt', 'yuv420p',
   '-sc_threshold', '0',
@@ -24,22 +24,34 @@ const options = [
   '-level', '3.1',
   '-c:a', 'aac',
   '-b:a', '128k',
-  '-ar', '44100', // Audio sample rate
+  '-ar', 128000 / 4,
   '-f', 'flv',
-  `rtmp://a.rtmp.youtube.com/live2/`,
+  `rtmp://a.rtmp.youtube.com/live2/asasas-asaseuk-ssss-as6sm1-trrb-cxkt`,
 ];
 
 const ffmpegProcess = spawn("ffmpeg", options);
 
-ffmpegProcess.stdin.on('error', (err) => {
-  console.error('FFmpeg stdin error:', err);
+ffmpegProcess.stdout.on("data",(data)=>{
+  console.log(`ffmpeg stdout`,data)
+})
+
+ffmpegProcess.stdin.on("error", (err) => {
+  console.error("FFmpeg stdin error:", err);
+});
+ffmpegProcess.stderr.on("data", (data) => {
+  console.error("FFmpeg stderr error:", data);
+});
+ffmpegProcess.on("close", (code) => {
+  console.error("FFmpeg process exitedd code:", code);
 });
 
 io.on("connection", (socket) => {
   console.log(`Socket Connected-${socket.id}`);
   socket.on("binarydata", (stream) => {
+    ffmpegProcess.stdin.write(stream, (err) => {
+      console.log("Err", err);
+    });
     console.log(`Binary Stream Comming...${stream}`);
-    // You may write the binary stream to ffmpegProcess.stdin here
   });
 });
 
